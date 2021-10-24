bb9bdb815d58374f103901e88afd55ab13c85465
Vijay Shanmugam <vrswizards2015@gmail.com>
2021-10-24 19:28:28 -0400

    test:

--- .next/static/chunks/78e521c3.186046dfe3862aae7780.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[17],{v7au:function(t,a,n){"use strict";n.d(a,"a",(function(){return i}));var h=n("Lnxd");function i(t){return Object(h.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"}}]}]})(t)}}}]);
